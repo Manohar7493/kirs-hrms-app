aa8c4298b02bf4057f2c6f1b6ede1cdd68c6e904
Manohar7493 <manohargorige7493@gmail.com>
2025-01-31 15:36:12 +0530

    updated

--- src/pages/admin/auth/AdminLogin.jsx
+++ src/pages/admin/auth/AdminLogin.jsx
@@ -2,12 +2,36 @@ import logo from '../../../assets/logo.png';
 function AdminLogin() {
     return (
         <main className="bg-blue-950 min-h-screen md:c0l-span-4">
-            <img className='max-w-50' src={logo}/>
-            <div className='flex item-center justify-center mt-10'>
+            <img className="max-w-50" src={logo} />
+            <div className="flex item-center justify-center mt-10">
                 <form className="w-full">
                     <div className="justify-items-center">
-                        <h1 className='text-4xl font'
+                        <h1 className="text-4xl font-bold text-white">Login</h1>
+                        <p className="text-lg text-white mt-3">Login to your account.</p>
+                        <div className="w-4/6 md:w-1/4 mt-6">
+                            <label htmlFor="E-mail-Address" className="block text-sm font-medium text-white">E-mail Address</label>
+                            <input type="email" name="E-mail-Address" className="block mt-2 border border-white bg-white py-2 w-full" autoComplete="E-mail-Address" />
+                        </div>
+                        <div className="w-5/6 md:w-1/4 mt-6">
+                            <label htmlFor="password" className="block text-sm font-medium text-white mt-2">Password</label>
+                            <input type="password" name="password" className="block mt-2 border border-white bg-white py-2 w-full" autoComplete="Password" />
+                        </div>
+                        <div className="container w-1/4 md:w-1/4 md:flex justify-between items-center mt-6">
+                            <div className="container flex justify-between items-center">
+                                <div className="">
+                                    <input type="checkbox" name="remember" id="remember" />
+                                    <label htmlFor="remember" className="text-white">Remember me</label>
+                                </div>
+                                <p className="text-white text-sm">Reset Password</p>
+                            </div>
+                        </div>
+                        <div className="w-5/6 md:w-1/4">
+                            <button type="submit" className="block mt-9 border bg-yellow-400 font-medium text-xl py-2 w-full">Signin</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </main>
     )
 }
-
 export default AdminLogin;
--- src/pages/employees/auth/EmployeeLogin.jsx
+++ src/pages/employees/auth/EmployeeLogin.jsx
@@ -1,8 +1,55 @@
+import {Link} from 'react-router-dom'
+import EmployeeLoginLogo from '../../../assets/employee-login-logo.png';
+
 function EmployeeLogin(){
     return(
-        <div>
-            EmployeeLogin
-        </div>
+        <main className="font-sans md:min-h-screen flex">
+            <div className="grid grid-col-1 md:grid-cols-2 w-full justify-items-center">
+                <div className="bg-white w-full px-5">
+                    <img src={EmployeeLoginLogo} alt="logo" className="w-3/12" />
+                    <div className="md:mt-8 md:ml-8">
+                        <div>
+                            <h1 className="font-bold text-white text-5xl pb-3">EmployeeLogin</h1>
+                            <p className="text-xl text-white">Employee Login to your account. </p>
+                        </div>
+                        <div className="md:mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                            <div className="sm:col-span-4">
+                                <label htmlFor="E-mail-Address" className="block text-lg font-medium text-blue-900">E-mail Address</label>
+                                <div className="mt-2">
+                                    <div className="flex item-center rounded-md bg-white pl-3 outline outline-1-outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outlinr-offset-2">
+                                        <input type="email" name="E-mail-Address" id="E-mail-Address" className="block border-none rounded min-w-0 grow py-2.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="Enter Your Email" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="md:mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                            <div className="sm:col-span-4">
+                                <label htmlFor="password" className="block text-lg font-medium text-blue-900">E-mail Address</label>
+                                <div className="mt-2">
+                                    <div className="flex item-center rounded-md bg-white pl-3 outline outline-1-outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outlinr-offset-2">
+                                        <input type="password" name="password" id="password" className="block border-none rounded min-w-0 grow py-2.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="Enter Your Password" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container grid grid-cols-2 ml-5 p-2">
+                        <div>
+                            <input type="checkbox" name="remember" id="remember" />
+                            <label htmlFor="remember" className="text-gray-500 text-lg">Remember me</label>
+                        </div>
+                        <p className="text-lg text-blue-950 font-semibold">Reset Password</p>
+                    </div>
+                    <div className="md:ml-8 md:pr-6 md:w-4/6">
+                        <button className="border-none rounded text-white bg-blue-950 text-center w-full py-3 mt-5">Signin</button>
+                    </div>
+                    <p className="text-gray-500 text-lg lg:pl-19 pt-5">Don't have an account yet?<link to='/EmployeeRegister' className="text-blue-950 font-semibold">Join KRIS today.</link></p> 
+                </div>
+                <div className="md:min-h-screen bg-blue-950">
+                    <h1 className="text-white text-5xl px-9 mt-16 md:mt-80 font-semiblod">Manage all <span className="text-yellow-500">HR Operations</span>from the comfort of your home</h1>
+                </div>
+            </div>
+        </main>
     )
 }
 export default EmployeeLogin;
